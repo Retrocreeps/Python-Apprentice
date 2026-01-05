c475706060ac3a503656d310b28f557bbf82ec6a
Retrocreeps <AndrewHSM999@gmail.com>
2026-01-05 19:30:18 +0000

    message

--- lessons/10_Turtles/60_More_Turtle_Programs/20_More_Turtle_programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/20_More_Turtle_programs.py
@@ -5,3 +5,11 @@ from the section "Change the Turtle Image"
 Then change the code so that the turtle has a different image ( look in the 'images'
 directory ) and moves to the corners of the screen in a square pattern. 
 """
+
+
+import turtle
+turtle.Turtle()
+
+tina = turtle.Turtle()
+turtle.setup(600,600,0,0) 
+
--- lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
+++ lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
@@ -13,17 +13,65 @@ t = ... # Create a turtle named t
 # For instance, you can make it go left 30 degrees, then forward 50 pixels, 
 # then right 60 degrees, then forward 100 pixels. Make any shape you like.
 
-def make_a_shape(t):
-    """Make a shape with turtle t. Make it go left or right or forward"""    
-    ...
+
+    
+
 
 # 2) Call make_a_shape() in a loop to make the turtle draw a spiral.
 # For instance, you can call make_a_shape() 100 times to make a spiral with 100 shapes.
 # The second ... in the for loop should be the number of shapes you want to make, 
 # for example 100, or it could use islice(), cycle(), or a list of numbers.
 
-num_shapes = ...
 
-for i in range(...):
-    make_a_shape(t)
-    t.right(360/num_shapes)
+
+
+
+import turtle
+turtle.setup(600,600,0,0)
+
+tina = turtle.Turtle()
+tina.shape('turtle')
+tina.speed(3)
+
+for i in range(40): 
+    tina.pencolor("red")
+    tina.forward(30)
+    tina.pencolor ("orange")
+    tina.forward(30)
+    tina.pencolor("yellow")
+    tina.forward(30)
+    tina.pencolor("green")
+    tina.forward(30)
+    tina.pencolor("lime")
+    tina.forward(30)
+    tina.pencolor("teal")
+    tina.forward(30)
+    tina.left(115)
+    tina.pencolor("cyan")
+    tina.forward(30)
+    tina.pencolor("blue")
+    tina.forward(30)
+    tina.pencolor("aqua")
+    tina.forward(30)
+    tina.pencolor("purple")
+    tina.forward(30)
+    tina.pencolor("magenta")
+    tina.forward(30)
+    tina.pencolor("pink")
+    tina.forward(30)
+    tina.left(115)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.exitonclick()
