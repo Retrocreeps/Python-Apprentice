8d19459d89a6e4198335be199f390ded4fe5d66c
Retrocreeps <AndrewHSM999@gmail.com>
2026-04-27 18:30:44 +0000

    lists

--- lessons/30_Loops/040_Crazy_Tina.py
+++ lessons/30_Loops/040_Crazy_Tina.py
@@ -11,25 +11,42 @@ more help
 """
 
 import turtle                           # Tell Python we want to work with the turtle
-turtle.setup(600,600,0,0)               # Set the size of the window
+turtle.setup(1000,1000,0,0)               # Set the size of the window
 
 tina = turtle.Turtle()                  # Create a turtle named tina
 
 tina.shape('turtle')                    # Set the shape of the turtle to a turtle
 tina.speed(2)                           # Make the turtle move as fast, but not too fast. 
 
-forwards = [ ... ]
-lefts = [ ... ]
-colors = [  ... ]
+forwards = [ 100, 50, 25, 75, 125, 100, 50, 100 ]
+lefts = [ 140, 90, 45, 60, 120, 30, 180, 90 ]
+colors = [ "red", "blue", "green", "yellow", "purple", "orange", "pink", "cyan" ] 
 
 for  i in range(8):
 
-    forward = ...
-    left = ...
-    color = ...
+    forward = forwards[i]
+    left = lefts[i]
+    color = colors[i]
 
     tina.color(color)
     tina.forward(forward)
     tina.left(left)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 turtle.exitonclick()  
